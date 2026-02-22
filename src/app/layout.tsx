b139b35f0bc20717c3d8751245c2f9b801e0924e
Dhia uddin <dhiauddin2010@gmail.com>
2026-02-22 07:15:40 +0000

    I am building a web app using Next.js and Firebase. My app currently sho

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M21 17a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z' fill='white' /><text x='11' y='16' font-family='sans-serif' font-size='12' font-weight='bold' fill='%233F51B5'>C</text></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
