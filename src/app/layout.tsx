dfe7ec1a9ba4f0a3a083ac7c6773832a844a59fd
Dhia uddin <dhiauddin2010@gmail.com>
2026-02-22 06:49:47 +0000

    I mean, in the tab there will be a logo here. Here is the logo for the t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%23656af3' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M12 22a10 10 0 0 0 10-10h-2a8 8 0 0 1-8 8v2Z'/><path d='M22 12a10 10 0 0 0-10-10v2a8 8 0 0 1 8 8h2Z'/><path d='M12 2a10 10 0 0 0-10 10h2a8 8 0 0 1 8-8V2Z'/></svg>" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%23656af3' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z'></path><path d='M15.5 9A3.5 3.5 0 0 0 12 12.5A3.5 3.5 0 0 0 15.5 16'></path></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
