ca046e563d9f0082c8a31cbba3c5476cb032c78f
Dhia uddin <dhiauddin2010@gmail.com>
2026-02-22 06:47:04 +0000

    in the tab there will be a logo for convo here is the logo:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%23656af3' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M12 22a10 10 0 0 0 10-10h-2a8 8 0 0 1-8 8v2Z'/><path d='M22 12a10 10 0 0 0-10-10v2a8 8 0 0 1 8 8h2Z'/><path d='M12 2a10 10 0 0 0-10 10h2a8 8 0 0 1 8-8V2Z'/></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
